aa1d2960cbdf29b3c7320ae1e666c10c762a813f
Vinh Hoang Nhu (FE FGW DN) <VinhHN3@fe.edu.vn>
2021-01-12 13:44:00 +0700

    Implement Delete action of TodosController

--- GCD0704.AppDev/Controllers/TodosController.cs
+++ GCD0704.AppDev/Controllers/TodosController.cs
@@ -1,6 +1,4 @@
 ﻿using GCD0704.AppDev.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -8,23 +6,37 @@ namespace GCD0704.AppDev.Controllers
 {
 	public class TodosController : Controller
 	{
+		private ApplicationDbContext _context;
+		public TodosController()
+		{
+			_context = new ApplicationDbContext();
+		}
 		// GET: Todos
-		List<Todo> todos = new List<Todo>()
-			{
-				new Todo(1, "Kill John", "Kill Them All !!!", new DateTime(2008, 5, 1, 8, 30, 52)),
-				new Todo(2, "Kill Bill", "Kill ....", new DateTime(2008, 5, 1, 8, 30, 52)),
-				new Todo(3, "Kill Wick", "Kill ....", new DateTime(2008, 5, 1, 8, 30, 52))
-			};
 
 		public ActionResult Index()
 		{
+			var todos = _context.Todos.ToList();
 			return View(todos);
 		}
 
 		public ActionResult Details(int id)
 		{
-			var todo = todos.SingleOrDefault(t => t.Id == id);
+			var todo = _context.Todos.SingleOrDefault(t => t.Id == id);
 			return View(todo);
+		}
+
+		public ActionResult Delete(int id)
+		{
+			var todoInDb = _context.Todos.SingleOrDefault(t => t.Id == id);
+			if (todoInDb == null)
+			{
+				return HttpNotFound();
+			}
+
+			_context.Todos.Remove(todoInDb);
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
 		}
 	}
 }
--- GCD0704.AppDev/Models/Todo.cs
+++ GCD0704.AppDev/Models/Todo.cs
@@ -4,17 +4,11 @@ namespace GCD0704.AppDev.Models
 {
 	public class Todo
 	{
+
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public DateTime DueDate { get; set; }
 
-		public Todo(int id, string name, string description, DateTime dueDate)
-		{
-			Id = id;
-			Name = name;
-			Description = description;
-			DueDate = dueDate;
-		}
 	}
 }
